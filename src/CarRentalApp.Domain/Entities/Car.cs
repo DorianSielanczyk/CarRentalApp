2bdd2c44b0e24133a1080f8aee9431ffc189c800
DorianSielanczyk <sielanczykdorian@gmail.com>
2026-04-19 14:59:55 +0200

    feat: add date range filter for car availability on Home page

--- src/CarRentalApp.Domain/Entities/Car.cs
+++ src/CarRentalApp.Domain/Entities/Car.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarRentalApp.Domain.Entities
+﻿namespace CarRentalApp.Domain.Entities
 {
     public class Car
     {
